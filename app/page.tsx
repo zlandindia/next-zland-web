ae5b405ea9caab1cbebabcbc91285786d7029a48
Zland India <132743425+zlandindia@users.noreply.github.com>
2024-01-18 19:32:35 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <>
+      <div>
+        {/* Navigation Section */}
+        <Image
+          src="/images/zland_logo.png"
+          width={50}
+          height={50}
+          alt="Picture of the author"
+          className="mt-5 ml-5"
+        />
+      </div>
+    </>
+  );
 }
