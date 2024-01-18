4fdf0ff280cc4c67ab44536962610be7cecd0dca
Zland India <132743425+zlandindia@users.noreply.github.com>
2024-01-18 20:10:32 +0530

    push

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,18 @@ import Image from "next/image";
 export default function page() {
   return (
     <>
-      <div>
+      <div className="mt-5 grid grid-cols-2">
         {/* Navigation Section */}
         <Image
           src="/images/zland_logo.png"
           width={50}
           height={50}
           alt="Picture of the author"
-          className="mt-5 ml-5"
+          className="ml-5"
         />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-1 rounded-full items-end mr-5 mb-1.5 ml-2 mt-1.5">
+          Download App
+        </button>
       </div>
     </>
   );
